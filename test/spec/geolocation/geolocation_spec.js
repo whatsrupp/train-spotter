4f20842f6a0e16eca75711701b2ecc119e498140
alicespyglass <alicespyglass@gmail.com>
2017-05-09 12:02:57 +0100

    add geolocator without test

---
+++ test/spec/geolocation/geolocation_spec.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  describe('Finding user location', function () {
+    describe('Makes a call on the API', function () {
+      it('should return latitude and longitude', function () {
+        expect(true).toBe(true);
+      });
+    });
+  });
+})();
