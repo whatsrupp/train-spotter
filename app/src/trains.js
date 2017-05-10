ac72d9dd53b564edb2e6ee691794b2e1bb20776e
alicespyglass <alicespyglass@gmail.com>
2017-05-10 15:51:38 +0100

    Trains - return array of potential trains based on start location and destination

--- app/src/trains.js
+++ app/src/trains.js
@@ -1,41 +1,50 @@
 // Api for finding potential trains
 
-// Start-Station
-// destination
-// Date
-// Time
-// app_id
-// app_key
-
 // https://transportapi.com/v3/uk/train/station/LBG/2017-05-10/10:41/timetable.json?app_id=221cce2f&app_key=d209929236fc97196775650c2bdb639e&destination=CHX&train_status=passenger;
 var items = '';
 (function(exports) {
-  function GetTrains() {
+  function GetTrains( startStation, destination ) {
+
+    this.startStation = startStation;
+    this.destination = destination;
+    this.trainData = [];
 
   }
 
+// Initiate GetTrains constructor. Run trainURL to get potetial trains.
     GetTrains.prototype = {
 
       trainURL:  function() {
         var urlPartOne = "https://transportapi.com/v3/uk/train/station/";
-        var urlPartTwo = "/timetable.json?app_id=221cce2f&app_key=d209929236fc97196775650c2bdb639e&destination=";
+        var urlPartTwo = "/timetable.json?app_id=221cce2f&app_key=d209929236fc97196775650c2bdb639e&calling_at=";
         var urlPartThree = "&train_status=passenger";
-        var startStation = 'LBG';
-        var dateNow = '/2017-05-10';
-        var timeNow = '/10:41';
-        var destination = 'CHX';
-        var defaultURL = urlPartOne + startStation + dateNow + timeNow + urlPartTwo + destination + urlPartThree;
-        $.get(defaultURL, function(data) {
-          debugger;
-        });
+        var startStation = this.startStation;
+        // var timeNow = '/10:41';
+        var destination = this.destination;
+        var defaultURL = urlPartOne + startStation + urlPartTwo + destination + urlPartThree;
 
-
-
-
-
+        return defaultURL
       },
-      add: function() {
-        console.log('hi');
+
+      getPotentialTrains: function() {
+
+        defaultURL = this.trainURL();
+        $.ajax({
+          url: defaultURL,
+          success: function(data) {
+            var trainData = data.departures;
+            this.setTrainData(trainData)
+          }.bind(this)
+        })
+      },
+
+      setTrainData: function(data) {
+        this.trainData = data
+      },
+
+
+      setStartStation: function(startStation) {
+        this.startStation = startStation;
       }
     };
 
