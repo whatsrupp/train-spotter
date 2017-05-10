a1a11982ea992b9bd4fd06e83cd5be8b9606045d
Nick Rupp <nickrupp95@gmail.com>
2017-05-10 17:39:48 +0100

    Use alternative location api

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,4 @@
 // -0.086092, 51.5077
-var geolocator = new Geolocator();
 var stationFinder = new StationFinder();
+var geolocator = new Geolocator(stationFinder);
 var initialize = new Initializer(geolocator, stationFinder);
