789232a22ff41b81883e70fe0cd0c78b9f7de157
alicespyglass <alicespyglass@gmail.com>
2017-05-11 14:36:07 +0100

    display info can be called from Journey object

--- app/src/journey.js
+++ app/src/journey.js
@@ -2,15 +2,28 @@
 
   function Journey(){
     this.stopOfInterest = '';
-    // this.finalDestination
-    // this.operator
-    // this.aimedArrivalTime
+    this.finalDestination
+    this.operator
+    this.ArrivalTime
   }
 
   Journey.prototype = {
     updateStopOfInterest: function(stopOfInterest) {
       this.stopOfInterest = stopOfInterest;
+    },
+
+    updateFinalDestination: function(finalDestination) {
+      this.finalDestination = finalDestination;
+    },
+
+    updateOperator: function(operator) {
+      this.operator = operator;
+    },
+
+    updateArrivalTime: function(arrivalTime) {
+      this.arrivalTime = arrivalTime;
     }
+
   };
 
   exports.Journey = Journey;
