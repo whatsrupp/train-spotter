b51b38cc858b1c412903bb335514aeba817fa303
Ruan Odendaal <ruanodendaal@gmail.com>
2017-05-11 12:44:15 +0100

    update travel info on click

--- app/src/index.js
+++ app/src/index.js
@@ -4,17 +4,39 @@ var geolocator = new Geolocator(stationFinder);
 var initialize = new Initializer(geolocator, stationFinder);
 
 // -0.086092, 51.5077
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function() {
   dest = new Destination();
+  getDetails();
 
-  function captureUserDestination() {
+  function getDetails() {
     document.getElementById('create').addEventListener('click', function(clickEvent) {
       clickEvent.preventDefault();
-      dest.saveDestination(document.getElementById('destination').value);
-      // below for testing purposes only
-      $('.usr-destination').text(dest.getText());
+      captureUserDestination();
     });
   };
 
-  captureUserDestination()
+  function captureUserDestination() {
+    dest.saveDestination(document.getElementById('destination').value);
+    updateInfo();
+    switchView();
+  };
+
+  function updateInfo() {
+    $('#usr-destination').append(dest.getText());
+    // get train operator
+    $('#train-operator').append('Thameslink');
+    // get last stationFinder
+    $('#last-station').append('London St Pancras');
+    // get arrival time
+    $('#arrival-time').append('18:56');
+    // get terminus
+    $('#terminus-station').append('Bedford');
+  }
+
+  function switchView() {
+    $('#train-info').toggleClass('hidden');
+    $('#finder').toggleClass('hidden');
+  };
+
+
 });
